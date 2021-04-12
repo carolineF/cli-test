ffa7ac6d7203634b59c3f81d046923de5a485a68
vermouth <vermouth_fee@outlook.com>
2021-04-12 12:37:12 +0800

    add CLI update/remove/list Customer from db

--- index.js
+++ index.js
@@ -8,28 +8,50 @@ const db = mongoose.connect('mongodb://localhost:27017/customercli', {
 });
 
 const Customer = require('./models/customer');
-const addCustomer = customer => {
-  Customer.create(customer).then(customer => {
+const addCustomer = async customer => {
+  await Customer.create(customer).then(customer => {
     console.info('New Customer Added')
-    db.close();
-  }).catch(err =>  {
-    console.log(chalk.red(err))
   })
+  mongoose.disconnect();
 }
 
-const findCustomer = name => {
+const findCustomer = async name => {
   const search = new RegExp(name, 'i');
-  Customer.find({$or: [{firstname: search}, { lastname: search}]})
+  await Customer.find({$or: [{firstname: search}, { lastname: search}]})
     .then(customer => {
       console.info(customer);
       console.info(`${customer.length} matches`)
-      db.close();
-    }).catch(err =>  {
-    console.log(chalk.red(err))
+    })
+  mongoose.disconnect();
+}
+
+const updateCustomer = async (_id, customer) => {
+  await Customer.updateOne({_id}, customer).then(customer => {
+    console.info('Customer Update');
   })
+  mongoose.disconnect();
+}
+
+const removeCustomer = async _id => {
+  await Customer.deleteOne({_id})
+    .then(customer => {
+      console.info('Customer Removed');
+    })
+  mongoose.disconnect();
+}
+
+const listCustomers = async () => {
+  await Customer.find().then(customers => {
+    console.info(customers);
+    console.info(`${customers.length} matches`)
+  })
+  mongoose.disconnect();
 }
 
 module.exports = {
   addCustomer,
-  findCustomer
+  findCustomer,
+  updateCustomer,
+  removeCustomer,
+  listCustomers
 }
